aa11f463f8a3003ad5787bbbf27e027702f120d6
Zachary Denham <zdenham@>
2016-07-24 17:57:16 -0700

    initial commit

---
+++ app/src/main/java/scope/todoapp/TodoMain.java
@@ -0,0 +1,13 @@
+package scope.todoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TodoMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo_main);
+    }
+}
